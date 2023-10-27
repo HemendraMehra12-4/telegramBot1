858c522367249c22efd9b1be5936dbb54194128f
aur1cL0rd <hmehra160@gmail.com>
2023-10-28 00:01:11 +0530

    modified main.py file

--- main.py
+++ main.py
@@ -59,29 +59,27 @@ async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f'Update {update} caused error {context.error}')
 
 
+print('Welcome to the botOfEchem...') 
+    
+app= Application.builder().token(TOKEN).build()
+    
+#commands
+    
+app.add_handler(CommandHandler('start',start_command))
+app.add_handler(CommandHandler('help',help_command))
+app.add_handler(CommandHandler('custom',custom_command))
 
-if __name__=='___main__':
-    print('Welcome to the botOfEchem...') 
+#Messages
     
-    app= Application.builder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT,handle_message))
     
-    #commands
+#errors
     
-    app.add_handler(CommandHandler('start',start_command))
-    app.add_handler(CommandHandler('help',help_command))
-    app.add_handler(CommandHandler('custom',custom_command))
-
-    #Messages
+app.add_error_handler(error)
     
-    app.add_handler(MessageHandler(filters.TEXT,handle_message))
-    
-    #errors
-    
-    app.add_error_handler(error)
-    
-    #polls the bot
-    print('polling')
-    app.run_polling(poll_interval=3)    
+#polls the bot
+print('polling')
+app.run_polling(poll_interval=3)    
 
 
 
